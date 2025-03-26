eb5844d6c331848bf6ea1d9d17277c253a356b59
George <george.kagwe@student.moringaschool.com>
2025-03-26 14:13:15 +0300

    fetch lab done

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books') // Ensure you return this
+    .then(response => response.json())
+    .then(data => {
+      renderBooks(data); // Pass JSON data to renderBooks()
+    })
+    .catch(error => console.error("Error fetching books:", error));
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
